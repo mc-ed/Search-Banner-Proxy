c2b996ee0ac644269e99604c6f0ff9211110cea7
DJ Park <dongjae93@gmail.com>
2019-07-10 12:15:08 -0500

    docker added

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import SearchBar from './component/SearchBar.jsx'
+
+
+ReactDOM.render(<SearchBar/>, document.getElementById('searchbar'));
